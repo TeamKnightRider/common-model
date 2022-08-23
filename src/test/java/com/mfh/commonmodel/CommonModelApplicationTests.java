f24c569c30c8bf2d0232d0c0ac19ee24b8e1e80c
faysal <mfh.faysal.0@gmail.com>
2022-08-23 23:00:02 +0600

    -common model for all project has been created

---
+++ src/test/java/com/mfh/commonmodel/CommonModelApplicationTests.java
@@ -0,0 +1,12 @@
+package com.mfh.commonmodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonModelApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
