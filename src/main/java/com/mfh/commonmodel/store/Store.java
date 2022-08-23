9c5a53c419feab13899d47de2bbc6091e7e4a3da
faysal <mfh.faysal.0@gmail.com>
2022-08-24 00:18:21 +0600

    -test removed -spring boot plugin removed which causes wrong location of classes in jar dependency

---
+++ src/main/java/com/mfh/commonmodel/store/Store.java
@@ -0,0 +1,51 @@
+package com.mfh.commonmodel.store;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+
+import com.mfh.commonmodel.core.CustomIdGenerator;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "tStore")
+@ToString(of = "name")
+public class Store implements Serializable {
+
+  @Serial
+  private static final long serialVersionUID = -5894136098419536213L;
+  @Id
+  @GeneratedValue(generator = "customGenerator", strategy = GenerationType.SEQUENCE)
+  @GenericGenerator(name = "customGenerator",
+      strategy = CustomIdGenerator.CLASS_REFERENCE,
+      parameters = {@Parameter(name = CustomIdGenerator.ENTITY_PREFIX, value = "store-")})
+  @Column(name = "rid", unique = true)
+  private String rid;
+
+  @Column(name = "name", nullable = false)
+  private String name;
+
+  @Column(name = "addressId")
+  private String addressId;
+
+  @Column(name = "createdOn", nullable = false)
+  private LocalDateTime createdOn;
+
+  @Column(name = "deleted", nullable = false)
+  private boolean deleted;
+}
